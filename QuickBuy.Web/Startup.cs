febb8087eeaf4672074f1fbba3ac25fe356ce853
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-03 18:55:32 -0300

    Controller

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuickBuy.Dominio.Contratos;
 using QuickBuy.Repositorio.Contexto;
+using QuickBuy.Repositorio.Repositorios;
 
 namespace QuickBuy.Web
 {
@@ -30,6 +32,9 @@ namespace QuickBuy.Web
             services.AddDbContext<QuickBuyContexto>(option =>
                                                    option.UseLazyLoadingProxies()
                                                    .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
