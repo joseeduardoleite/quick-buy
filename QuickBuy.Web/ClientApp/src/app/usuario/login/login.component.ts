8a3f4ff26856693a8b39aca7653bb73def15a824
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-14 02:40:37 -0300

    rotas

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,20 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../model/usuario";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public usuario;
- 
-  constructor() {
-    this.usuario = new Usuario()
+  public returnUrl: string;
+
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl']
+    this.usuario = new Usuario();
   }
 
   entrar() {
     if (this.usuario.email == "eduardo@teste.com" && this.usuario.senha == "eduardo1201") {
+      sessionStorage.setItem("usuario-aautenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
   }
 
