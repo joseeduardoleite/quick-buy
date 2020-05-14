f4c4f70318cb831f194d26d4fc40b4cf0c9cc79d
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-14 17:04:22 -0300

    servicos

---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Email == "eduardo@teste.com" && usuario.Senha == "eduardo")
+                return Ok(usuario);
+
+                return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
