7ad4749b619ab347422f1c3b48f07b60c997b213
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-17 23:10:36 -0300

    cadastro

--- QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,1 +1,12 @@
-﻿
+import { Component } from "@angular/core";
+import { templateJitUrl } from "@angular/compiler";
+
+@Component({
+  selector: "cadastro-usuario",
+  templateUrl: "./cadastro.usuario.component.html",
+  styleUrls: ["./cadastro.usuario.component.css"]
+})
+
+export class CadastroUsuarioComponent {
+
+}
