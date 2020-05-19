6aafc71dc1d7aa43dad2acd1f5e46396c5d452b9
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-19 00:44:52 -0300

    produto

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,16 +1,32 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { Produto } from "../model/produto";
+import { ProdutoServico } from "../servicos/produto/produto.servico";
 
 @Component({
   selector: "app-produto",
   template: "./produto.component.html",
   styleUrls: ["./produto.component.css"]
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
+  public produto: Produto;
 
-  public nome: string;
-  public liberadoParaVenda: boolean;
+  constructor(private produtoServico: ProdutoServico) {
 
-  public obterNome(): string {
-    return "Samsung";
+  }
+
+  ngOnInit(): void {
+    this.produto = new Produto();
+  }
+
+  public cadastrar() {
+    this.produtoServico.cadastrar(this.produto)
+      .subscribe(
+        produtoJson => {
+          console.log(produtoJson);
+        },
+        e => {
+          console.log(e.error);
+        }
+      );
   }
 }
