de0ca3b22c82a5b143866f7e400c84a3605dd949
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-20 20:22:20 -0300

    service

--- QuickBuy.Web/ClientApp/src/app/servicos/produto/produto.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/produto/produto.servico.ts
@@ -41,4 +41,10 @@ export class ProdutoServico implements OnInit {
   public obterProduto(produtoId: number): Observable<Produto> {
     return this.http.get<Produto>(this._baseUrl + "api/produto");
   }
+
+  public enviarArquivo(arquivoSelecionado: File): Observable<boolean> {
+    const formData: FormData = new FormData();
+    formData.append("arquivoEnviado", arquivoSelecionado, arquivoSelecionado.name);
+    return this.http.post<boolean>(this._baseUrl + "api/produto/enviarArquivo", arquivoSelecionado);
+  }
 }
