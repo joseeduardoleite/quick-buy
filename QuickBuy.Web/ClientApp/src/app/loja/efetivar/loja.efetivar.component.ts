382eae70023c09bb7aca2cd6c3848b1cdaa75865
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-22 20:11:34 -0300

    efetivar

---
+++ QuickBuy.Web/ClientApp/src/app/loja/efetivar/loja.efetivar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "loja-efetivar",
+  templateUrl: "./loja.efetivar.component.html",
+  styleUrls: ["./loja.efetivar.component.css"]
+})
+
+export class LojaEfetivarComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+  constructor() {
+
+  }
+
+}
