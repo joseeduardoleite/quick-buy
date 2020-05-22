0d20a523b6c97115a5ce27c4fd270cc5761f9630
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-21 23:53:57 -0300

    pesquisa

--- QuickBuy.Web/ClientApp/src/app/produto/pesquisa/pesquisa.produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/pesquisa/pesquisa.produto.component.ts
@@ -1,1 +1,14 @@
-﻿
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "pesquisa-produto",
+  templateUrl: "./pesquisa.produto.component.html",
+  styleUrls: ["./pesquisa.produto.component.css"]
+})
+export class PesquisaProdutoComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+}
